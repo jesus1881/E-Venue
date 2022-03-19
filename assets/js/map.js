a524328cd6fdaac3a38a5346ea9cd7dcf99070ea
Okens <Ng.Eric314@gmail.com>
2022-03-18 23:10:31 -0700

    Updating map ratio to 3:4

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,7 +6,7 @@ function getMaps(zoom, latitude, longitude){
 if (zoom >= 20){
     zoom = 19
 }
-    var apiUrl = `https://api.tomtom.com/map/1/staticimage?key=${config.maps.TEMP_KEY}&zoom=${zoom}&center=${longitude},${latitude}&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=en-US`
+    var apiUrl = `https://api.tomtom.com/map/1/staticimage?key=${config.maps.TEMP_KEY}&zoom=${zoom}&center=${longitude},${latitude}&format=jpg&layer=basic&style=main&width=800&height=600&view=Unified&language=en-US`
     return apiUrl;
     
    
